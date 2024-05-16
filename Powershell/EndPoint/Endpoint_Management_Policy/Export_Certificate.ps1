eb7c56aac499c8a7df4f5f1f270cc572e2a8c4be
rsaddul <52894448+rsaddul@users.noreply.github.com>
2024-05-16 11:05:17 +0100

    Delete Powershell/EndPoint directory

--- Powershell/EndPoint/Endpoint_Management_Policy/Export_Certificate.ps1
+++
@@ -1,2 +0,0 @@
-﻿$cert = Get-AuthenticodeSignature "C:\Users\EduthingAdmin\AppData\Local\Apps\2.0\M8MXH6PP.86H\DBC6ORCT.2GG\conn..tion_abc359f7820ce320_0006.000b_f1a77bad889f7de0\ConnectChildcare.exe" | Select-Object -ExpandProperty SignerCertificate
-Export-Certificate -Cert $cert -FilePath "C:\ConnectChildcare.cer" -Type CERT
--- Powershell/EndPoint/Endpoint_Management_Policy/Export_FileHash.ps1
+++
@@ -1,9 +0,0 @@
-﻿Get-FileHash "File location + file" | select-object Hash
-
-########
-Example:
-Get-FileHash "C:\Users\Joost\Downloads\7z2201-x64.exe" | select-object Hash
-
-Hash
-----
-B055FEE85472921575071464A97A79540E489C1C3A14B9BDFBDBAB60E17F36E4
