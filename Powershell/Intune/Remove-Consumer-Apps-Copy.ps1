595e25ef5d06b97528511d47ac7f48d88b0767e2
rsaddul <52894448+rsaddul@users.noreply.github.com>
2026-01-22 14:19:32 +0000

    Delete Powershell/Intune/Remove-Consumer-Apps-Copy.ps1

--- Powershell/Intune/Remove-Consumer-Apps-Copy.ps1
+++
@@ -1,3 +0,0 @@
-﻿If ((Test-Path C:\Remove-Consumer-Apps.ps1) -eq $false) {Copy-Item "\\resource.uc\netlogon\OSD\Remove-Consumer-Apps.ps1" -Destination "C:\Remove-Consumer-Apps.ps1"}
-$runpath = "C:\Remove-Consumer-Apps.ps1"
-Start-Process -filepath $runpath
