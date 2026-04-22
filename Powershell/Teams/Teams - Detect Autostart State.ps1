c6bfae85895a228ea97ac8f326581b688b31bf41
rsaddul <52894448+rsaddul@users.noreply.github.com>
2026-04-22 15:29:14 +0100

    Add files via upload

---
+++ Powershell/Teams/Teams - Detect Autostart State.ps1
@@ -0,0 +1,17 @@
+﻿$newTeamsKey = "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\MSTeams_8wekyb3d8bbwe\TeamsTfwStartupTask"
+
+if (!(Test-Path $newTeamsKey)) {
+    Write-Output "New Teams not installed"
+    exit 0
+}
+
+$state = Get-ItemPropertyValue -Path $newTeamsKey -Name "State" -ErrorAction SilentlyContinue
+
+if ($state -eq 2) {
+    Write-Output "New Teams autostart enabled"
+    exit 0
+}
+else {
+    Write-Output "New Teams autostart disabled"
+    exit 1
+}
---
+++ Powershell/Teams/Teams - Enable Autostart.ps1
@@ -0,0 +1,31 @@
+﻿# Teams startup registry path
+$teamsStartupKey = "HKCU:\Software\Classes\Local Settings\Software\Microsoft\Windows\CurrentVersion\AppModel\SystemAppData\MSTeams_8wekyb3d8bbwe\TeamsTfwStartupTask"
+
+# Desired values
+$enabledStartupState = 2
+$userApprovedStartup = 1
+
+if (Test-Path $teamsStartupKey) {
+
+    $currentStartupState = Get-ItemPropertyValue -Path $teamsStartupKey -Name "State" -ErrorAction SilentlyContinue
+
+    if (($currentStartupState -eq 0) -or ($currentStartupState -eq 1)) {
+
+        Set-ItemProperty -Path $teamsStartupKey -Name "State" -Value $enabledStartupState -ErrorAction SilentlyContinue
+        Set-ItemProperty -Path $teamsStartupKey -Name "UserEnabledStartupOnce" -Value $userApprovedStartup -ErrorAction SilentlyContinue
+
+        Write-Host "Microsoft Teams auto-start has been enabled."
+
+    }
+    else {
+
+        Write-Host "Microsoft Teams auto-start is already enabled."
+
+    }
+
+}
+else {
+
+    Write-Host "Microsoft Teams startup registry key not found."
+
+}
